1947f80c0546aba99b695c0923a8d26aa04a0282
Wan Youwen <1756429307@qq.com>
2018-07-07 23:35:10 +0800

    Update KeyFrame.cc

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -12,35 +12,39 @@ using namespace GVars3;
 
 /**
  * @brief Perpares a Keyframe from an image.
- *        Generates pyramid levels, does FAST detection, etc.
+ *        1. 生成各级金字塔图像 Generates pyramid levels, 
+ *        2. 对每层进行FAST角点检测 does FAST detection, etc.
+ *        3. 建立角点查找表，加快查找，对每一行的角点创建查找表LUT 加速查找邻居角点 Generate row look-up-table for the FAST corner points
  * @param im gray scale image
  */
 void KeyFrame::MakeKeyFrame_Lite(BasicImage<byte> &im)
 {
-    aLevels[0].im.resize(im.size());
+    aLevels[0].im.resize(im.size());// 初始层
     copy(im, aLevels[0].im);
 
     for(int i=0; i<LEVELS; i++) {
+// 1. 生成金字塔图像，上一层下采样得到下一层图像
         Level &lev = aLevels[i];
         if (i != 0) {
-            lev.im.resize(aLevels[i - 1].im.size() / 2);
-            halfSample(aLevels[i - 1].im, lev.im);
+            lev.im.resize(aLevels[i - 1].im.size() / 2);// 尺寸减半
+            halfSample(aLevels[i - 1].im, lev.im);// 上一层下采样，得到下一层图像
         }
+// 2. FAST角点 检测，对每一层进行 FAST角点 检测
         // .. and detect and store FAST corner points.
         // I use a different threshold on each level; this is a bit of a hack
         // whose aim is to balance the different levels' relative feature densities.
-        lev.vCorners.clear();
+        lev.vCorners.clear();// 每一层对应的 FAST 角点
         lev.vCandidates.clear();
         lev.vMaxCorners.clear();
-        if (i == 0)
+        if (i == 0)// 第0层 图像
             fast_corner_detect_10(lev.im, lev.vCorners, 10);
-        if (i == 1)
+        if (i == 1)// 第1层 图像
             fast_corner_detect_10(lev.im, lev.vCorners, 15);
-        if (i == 2)
+        if (i == 2)// 第2层 图像
             fast_corner_detect_10(lev.im, lev.vCorners, 15);
-        if (i == 3)
+        if (i == 3)// 第4层 图像
             fast_corner_detect_10(lev.im, lev.vCorners, 10);
-
+// 3. 建立角点查找表，加快查找，对每一行的角点创建查找表LUT 加速查找邻居角点
         // Generate row look-up-table for the FAST corner points: this speeds up
         // finding close-by corner points later on.
         unsigned int v = 0;
@@ -48,7 +52,7 @@ void KeyFrame::MakeKeyFrame_Lite(BasicImage<byte> &im)
         for (int y = 0; y < lev.im.size().y; y++) {
             while (v < lev.vCorners.size() && y > lev.vCorners[v].y)
                 v++;
-            lev.vCornerRowLUT.push_back(v);
+            lev.vCornerRowLUT.push_back(v);// 每一行的查找表
         }
     }
 }
